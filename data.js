050f067f266b3f83aff72087672d04829d76038c
Franek-86 <fraleccese86@gmail.com>
2023-03-17 22:51:21 +0100

    last commit

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const articles = [
   {
-    img: "https://grifomultimedia.it/wp-content/uploads/2022/11/FNC-ANPAL-BLOG.jpg",
+    img: "https://tecfor.eu/wp-content/uploads/2019/12/Grifo-Multimedia-Le-strategie-ehealth.jpg",
     title: "Fondo nuove competenze: proroga delle domande al 27 marzo",
     date: "Marzo 08, 2023",
     text: `Il Fondo Nuove Competenze rappresenta un’opportunità per i lavoratori di acquisire nuove o ma... `,
     link: `https://grifomultimedia.it/fondo-nuove-competenze-proroga-delle-domande-al-27-marzo/`,
   },
   {
-    img: "https://static.cegos.it/wp-content/uploads/2020/04/09165743/preview-image-7-comp-infogr.jpg",
+    img: "https://poloinnovazioneict.org/wp-content/uploads/2021/01/Grifo-Multimedia-Il-Caso-Credit-Agricole-04-scaled.jpg",
     title: "Le competenze del futuro: quali sono e come acquisirle",
     date: "Marzo 16, 2023",
     text: `Il 2023 sarà un anno decisivo per il futuro del lavoro. La quarta rivoluzione industriale porterà ...`,
